c53b22a64a610c00204c01818505aebee1e4e468
raddevus <1radeutsch1@gmail.com>
2019-09-17 17:14:11 -0400

    Initial working prototype to prove that Electron can write to localStorage and use the sha256 library to generate the password and copy to the clipboard.  Currently runs on windows -- now can be used to test prototype on Linux.

--- main.js
+++ main.js
@@ -2,19 +2,23 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
+    // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js')
     }
   })
+
+  
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
